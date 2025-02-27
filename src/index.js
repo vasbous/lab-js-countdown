e5f4d3922e8ce84d6ce498bb007216908f46dfd7
Vassilis Bousbourelis <vasbousdev@gmail.com>
2025-02-27 20:57:16 +0100

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,38 +1,68 @@
 const DURATION = 10; // 10 seconds
+
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
-
-
 
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
+const startBtnElement = document.getElementById("start-btn");
+const timeElement = document.getElementById("time");
+// Affect the DURATION
+timeElement.innerText = remainingTime;
 
-
-
+startBtnElement.addEventListener("click", () => {
+  startCountdown();
+});
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
-  console.log("startCountdown called!");
-
-
+  // console.log("startCountdown called!");
+  if (remainingTime == 10) {
+    showToast("⏰ Final countdown! ⏰");
+  }
+  startBtnElement.disabled = true;
   // Your code goes here ...
+  timer = setInterval(() => {
+    if (remainingTime > 0) {
+      if (remainingTime == 6) {
+        showToast("Start the engines! 💥");
+      }
+      timeElement.innerText = remainingTime - 1;
+      remainingTime--;
+    } else {
+      showToast("Lift off! 🚀");
+      clearInterval(timer);
+      startBtnElement.disabled = false;
+    }
+  }, 1000);
 }
-
-
-
 
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
-
+  const toastMessageElement = document.getElementById("toast-message");
+  toastMessageElement.innerText = message;
   // Your code goes here ...
-
-
-
+  const showToastElement = document.getElementById("toast");
+  showToastElement.classList.add("show");
+  setTimeout(() => {
+    showToastElement.classList.remove("show");
+    if (remainingTime <= 0) {
+      remainingTime = DURATION;
+      timeElement.innerText = remainingTime;
+    }
+  }, 3000);
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
-
+  const closeToastElement = document.getElementById("close-toast");
+  closeToastElement.addEventListener("click", () => {
+    showToastElement.classList.remove("show");
+    if (remainingTime <= 0) {
+      remainingTime = DURATION;
+      timeElement.innerText = remainingTime;
+    }
+  });
 }
